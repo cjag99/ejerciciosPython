b0aa426845dddea875b6bb3888c9ada081383657
Jose Enrique Aguilar <cjag99@gmail.com>
2025-12-01 20:40:09 +0100

    Update models

--- django/EjemploDjango/HolaMundo/models.py
+++ django/EjemploDjango/HolaMundo/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Author (models.Model):
+    name=models.CharField (verbose_name='Nombre', # etiqueta dentro de la tabla
+    max_length= 100,
+    default=''
+    )
+    last_name=models.CharField(verbose_name='Apellido',
+    max_length=150,
+    default='')
+    age=models.PositiveSmallIntegerField (verbose_name='Edad',
+    default=0
+    )
+    mail=models.EmailField (verbose_name='Correo Electrónico',
+    max_length=254, default='')
